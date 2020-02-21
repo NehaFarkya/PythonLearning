b6bc67ce2714497cdff17485268640d5db218622
Neha Farkya <nehafarkya.20@gmail.com>
2020-02-21 14:54:58 +0100

    nitial commit

---
+++ String.py
@@ -0,0 +1,5 @@
+first_name=input("enter the first name")
+middle_name=input("emter his middle nmae")
+last_name=input("enter his last name")
+
+print("your initials are",first_name[0],middle_name[0],last_name[0])
